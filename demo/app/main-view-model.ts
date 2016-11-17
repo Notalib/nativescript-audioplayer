56aa3914eb3e609ac6ddd56e5f3a23ddebfa017b
Daniel Freiling <dfg@nota.dk>
2016-11-17 17:36:57 +0100

    Updated demo

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -8,35 +8,44 @@ export class HelloWorldModel extends Observable implements PlaybackEventListener
 
   constructor() {
     super();
+    this.player = new AudioPlayer();
+    this.player.setPlaybackEventListener(this);
     setTimeout(() => {
       this.loadAndSetupPlaylist();
-      this.message = this.player.message;
+      this.message = 'Loading';
     }, 1000);
     setInterval(() => {
         if (this.player && this.player.isPlaying()) {
-            console.log(`Playing ${this.player.getCurrentPlaylistIndex()} @ ${this.player.getCurrentTime()} of ${this.player.getDuration()}`);
+            console.log(`Playing playlist UID (${this.player.getCurrentPlaylistUID()}). Index ${this.player.getCurrentPlaylistIndex()} @ ${this.player.getCurrentTime()} of ${this.player.getDuration()}`);
         }
     }, 1000);
   }
 
   public loadAndSetupPlaylist() {
-    let playlist = new Playlist();
-    playlist.tracks.push(new MediaTrack("http://www.moviesoundclips.net/download.php?id=3706&ft=mp3", "CoffeeSteam", "SoundSnap.com", "Short Test", null));
-    playlist.tracks.push(new MediaTrack("https://archive.org/download/George-Orwell-1984-Audio-book/1984-01.mp3", "1984", "George Orwell", "Del 1 af 4", "http://bookcover.nota.dk/714070_w140_h200.jpg"));
-    playlist.tracks.push(new MediaTrack("https://archive.org/download/George-Orwell-1984-Audio-book/1984-02.mp3", "1986", "George Orwell", "Del 2 af 4", "http://bookcover.nota.dk/714070_w140_h200.jpg"));
-    playlist.tracks.push(new MediaTrack("https://archive.org/download/George-Orwell-1984-Audio-book/1984-03.mp3", "1986", "George Orwell", "Del 3 af 4", "http://bookcover.nota.dk/714070_w140_h200.jpg"));
-    playlist.tracks.push(new MediaTrack("https://archive.org/download/George-Orwell-1984-Audio-book/1984-04.mp3", "1986", "George Orwell", "Del 4 af 4", "http://bookcover.nota.dk/714070_w140_h200.jpg"));
-    playlist.tracks.push(new MediaTrack("http://mean2u.rfshq.com/downloads/music/giveyouup.mp3", "Rick n' Roll", "Rick Astley", "album", null));
-    playlist.tracks.push(new MediaTrack("http://www.moviesoundclips.net/download.php?id=3706&ft=mp3", "CoffeeSteam2", "SoundSnap.com", "Short Test", null));
-    playlist.tracks.push(new MediaTrack("http://www.moviesoundclips.net/download.php?id=3706&ft=mp3", "CoffeeSteam3", "SoundSnap.com", "Short Test", null));
-    this.player = new AudioPlayer(playlist);
+    const playlistUID = "UID_12345";
+    if (this.player.getCurrentPlaylistUID() == playlistUID) {
+      console.log('Player already has playlist: '+ this.player.getCurrentPlaylistUID());
+    } else {
+      console.log('Loading new playlist. Old UID: '+ this.player.getCurrentPlaylistUID());
+      const playlist = new Playlist(playlistUID,
+        new MediaTrack("http://www.moviesoundclips.net/download.php?id=3706&ft=mp3", "CoffeeSteam", "SoundSnap.com", "Short Test", null),
+        new MediaTrack("https://archive.org/download/George-Orwell-1984-Audio-book/1984-01.mp3", "1984", "George Orwell", "Del 1 af 4", "http://bookcover.nota.dk/714070_w140_h200.jpg"),
+        new MediaTrack("https://archive.org/download/George-Orwell-1984-Audio-book/1984-02.mp3", "1986", "George Orwell", "Del 2 af 4", "http://bookcover.nota.dk/714070_w140_h200.jpg"),
+        new MediaTrack("https://archive.org/download/George-Orwell-1984-Audio-book/1984-03.mp3", "1986", "George Orwell", "Del 3 af 4", "http://bookcover.nota.dk/714070_w140_h200.jpg"),
+        new MediaTrack("https://archive.org/download/George-Orwell-1984-Audio-book/1984-04.mp3", "1986", "George Orwell", "Del 4 af 4", "http://bookcover.nota.dk/714070_w140_h200.jpg"),
+        //new MediaTrack("http://mean2u.rfshq.com/downloads/music/giveyouup.mp3", "Rick n' Roll", "Rick Astley", "album", null),
+        new MediaTrack("http://www.moviesoundclips.net/download.php?id=3706&ft=mp3", "CoffeeSteam2", "SoundSnap.com", "Short Test", null),
+        new MediaTrack("http://www.moviesoundclips.net/download.php?id=3706&ft=mp3", "CoffeeSteam3", "SoundSnap.com", "Short Test", null)
+      );
+      this.player.loadPlaylist(playlist);
+      this.player.setPlaybackEventListener(this);
+    }
       // "http://www.noiseaddicts.com/samples_1w72b820/4357.mp3",
       // "http://www.noiseaddicts.com/samples_1w72b820/3816.mp3"]);
     // this.player.addToPlaylist([
     //   "http://www.noiseaddicts.com/samples_1w72b820/202.mp3",
     //   "http://www.noiseaddicts.com/samples_1w72b820/4941.mp3",
     //   "http://mean2u.rfshq.com/downloads/music/giveyouup.mp3"]);
-    this.player.setPlaybackEventListener(this);
   }
 
   public play() {
@@ -74,11 +83,23 @@ export class HelloWorldModel extends Observable implements PlaybackEventListener
     // this.player.seekTo(Math.min(seekTo, this.player.getDuration()));
   }
 
+  public sleep() {
+    this.player.setSleepTimer(5000);
+  }
+
+  public cancelSleep() {
+    this.player.cancelSleepTimer();
+  }
+
   public reload() {
     this.player.release();
     setTimeout(() => {
-      this.loadAndSetupPlaylist();
-    });
+      this.player = new AudioPlayer();
+      this.player.setPlaybackEventListener(this);
+      this.player.isReady.then(() => {
+        this.loadAndSetupPlaylist();
+      });
+    }, 500);
   }
 
   public logDuration() {
