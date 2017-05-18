7c34f83038bb3f4d3ded7d9fa9e2b67c4aaa5f67
Daniel Freiling <dfg@nota.dk>
2017-05-18 12:17:10 +0200

    refactor: use closure callbacks in delegate and move private helpers

--- src/audioplayer.ios.ts
+++ src/audioplayer.ios.ts
@@ -14,65 +14,77 @@ class AudioPlayerDelegateImpl extends NSObject implements AudioPlayerDelegate {
     public latestCurrentDuration: number;
     public latestCurrentTime: number;
 
-    private player: TNSAudioPlayer;
+    public onTimeUpdate: (seconds: number) => void;
+    public onFinishedPlayingItem: (item: AudioItem) => void;
+    public onMetadataReceived: (item: AudioItem, data: NSArray) => void;
+    public onBufferingUpdate: (item: AudioItem, earliestBufferSec: number, latestBufferSec: number) => void;
+    public onStateChanged: (from: AudioPlayerState, to: AudioPlayerState) => void;
 
     static new(): AudioPlayerDelegateImpl {
         return <AudioPlayerDelegateImpl>super.new() // calls new() on the NSObject
     }
 
-    public withPlayer(player: TNSAudioPlayer): AudioPlayerDelegateImpl {
-        this.player = player;
-        return this;
-    }
-
     public audioPlayerDidChangeStateFromTo(audioPlayer: AudioPlayer, from: AudioPlayerState, state: AudioPlayerState): void {
-        this.player._iosPlayerChangedState(from, state);
+        if (this.onStateChanged) {
+            this.onStateChanged(from, state);
+        }
     }
 
 	public audioPlayerDidFindDurationFor(audioPlayer: AudioPlayer, duration: number, item: AudioItem): void {
-        console.log(`didFindDurationFor: ${item.title}: ${duration}`);
+        // console.log(`didFindDurationFor: ${item.title}: ${duration}`);
         this.latestCurrentDuration = duration;
     }
 
     public audioPlayerDidLoadEarliestLatestFor?(audioPlayer: AudioPlayer, earliest: number, latest: number, item: AudioItem): void {
-        console.log(`didLoadFor: ${item.title}, range: ${earliest} <-> ${latest}`);
         this.latestLoadedDuration = latest - earliest;
+        if (this.onBufferingUpdate) {
+            this.onBufferingUpdate(item, earliest, latest);
+        }
     }
 
 	public audioPlayerDidUpdateEmptyMetadataOnWithData(audioPlayer: AudioPlayer, item: AudioItem, data: NSArray): void {
-        console.log('didUpdateEmptyMetadata for '+ item.title);
+        // console.log('didUpdateEmptyMetadata for '+ item.title);
+        if (this.onMetadataReceived) {
+            this.onMetadataReceived(item, data);
+        }
     }
 
 	public audioPlayerDidUpdateProgressionToPercentageRead(audioPlayer: AudioPlayer, time: number, percentageRead: number): void {
         // console.log(`didUpdateProgress: ${time} - ${percentageRead}`);
         this.latestCurrentTime = time;
-        this.player._iosTimeUpdate(time);
+        if (this.onTimeUpdate) {
+            this.onTimeUpdate(time);
+        }
     }
 
     public audioPlayerWillStartPlaying(audioPlayer: AudioPlayer, item: AudioItem): void {
-        console.log('willStartPlaying '+ item.title);
+        // console.log('willStartPlaying '+ item.title);
+        this.latestCurrentTime = 0;
     }
 
     public audioPlayerFinishedPlaying?(audioPlayer: AudioPlayer, item: AudioItem): void {
-
+        // console.log('finishedPlaying '+ item.title);
+        if (this.onFinishedPlayingItem) {
+            this.onFinishedPlayingItem(item);
+        }
     }
 }
 
 export class TNSAudioPlayer extends CommonAudioPlayer 
 {
     public player: AudioPlayer;
-    
+
     private delegate: AudioPlayerDelegateImpl;
 
     private iosPlaylist: NSArray;
     private seekIntervalSeconds = 15;
-    
+
     constructor() {
         super();
     }
-    
+
     public isReady = Promise.resolve(true);
-    
+
     public preparePlaylist(playlist: Playlist) {
         this._log('preparePlaylist');
         if (!this.player) {
@@ -89,12 +101,17 @@ export class TNSAudioPlayer extends CommonAudioPlayer
 
     private setupAudioPlayer() {
         this.player = AudioPlayer.new();
-        this.delegate = AudioPlayerDelegateImpl.new().withPlayer(this);
+        this.delegate = AudioPlayerDelegateImpl.new();
+        this.delegate.onTimeUpdate = (time) => this._iosTimeUpdate(time);
+        this.delegate.onStateChanged = (from, to) => this._iosPlayerStateChanged(from, to);
+        this.delegate.onFinishedPlayingItem = (item) => this._iosFinishedPlayingItem(item);
+        this.delegate.onBufferingUpdate = (item, earliest, latest) => this._iosBufferingUpdate(item, earliest, latest);
+        //this.delegate.onMetadataReceived = (item, data) => this._iosMetadataReceived(item, data);
         this.player.delegate = this.delegate;
         this.player.bufferingStrategy = AudioPlayerBufferingStrategy.PlayWhenPreferredBufferDurationFull;
         this.player.preferredBufferDurationBeforePlayback = 10;
-        console.log(`Player: ${this.player}`);
-        console.log(`Delegate: ${this.delegate}`);
+        this._log(`Player: ${this.player}`);
+        this._log(`Delegate: ${this.delegate}`);
     }
 
     private getAudioItemForMediaTrack(track: MediaTrack): AudioItem {
@@ -114,13 +131,13 @@ export class TNSAudioPlayer extends CommonAudioPlayer
         }
         return arr;
     }
-    
+
     private getNSURL(urlString: string) {
         return NSURL.URLWithString(urlString);
     }
-    
+
     public addToPlaylist(track: MediaTrack) {}
-    
+
     public play() {
         try {
             if (this.player.state == AudioPlayerState.Paused) {
@@ -130,15 +147,15 @@ export class TNSAudioPlayer extends CommonAudioPlayer
                 this.player.playWithItemsStartAtIndex(this.iosPlaylist, 0);
             }
         } catch(err) {
-            console.log(`Err: ${err}`);
+            this._log(`Err: ${err}`);
         }
     }
-    
+
     public pause() {
         this._log('pause');
         this.player.pause()
     }
-    
+
     public stop() {
         this._log('stop');
         this.cancelSleepTimer();
@@ -180,30 +197,110 @@ export class TNSAudioPlayer extends CommonAudioPlayer
     }
 
     public getCurrentPlaylistIndex() {
-        return this.iosPlaylist.indexOfObject(this.player.currentItem);
+        if (this.iosPlaylist && this.player && this.player.currentItem) {
+            const result = this.iosPlaylist.indexOfObject(this.player.currentItem);
+            if (result != NSNotFound) {
+                return result;
+            }
+        }
+        return -1;
     }
 
     public seekTo(milisecs: number) {
-        console.log(`seekTo ${milisecs}ms`);
         this._iosSeekTo(milisecs);
     }
+
+    private _sleepTimer: number;
+    private _sleepTimerPaused: boolean = false;
+    private _sleepTimerMillisecsLeft: number = 0;
+    
+    public setSleepTimer(millisecs: number) {
+        this.cancelSleepTimer();
+
+        const countdownTick = 1000;
+        this._sleepTimerMillisecsLeft = millisecs;
+        this._sleepTimer = setInterval(() => {
+            if (!this._sleepTimerPaused && this.isPlaying()) {
+                this._sleepTimerMillisecsLeft = Math.max(this._sleepTimerMillisecsLeft - countdownTick, 0);
+                this._onPlaybackEvent(PlaybackEvent.SleepTimerChanged);
+            }
+            if (this._sleepTimerMillisecsLeft == 0) {
+                // Fade out volume and pause if not already paused.
+                if (this.isPlaying()) {
+                    // TODO: fade out volume
+                    //   this.fadeOutVolumeAndPause();
+                }
+                clearInterval(this._sleepTimer);
+                this._sleepTimer = undefined;
+            }
+        }, countdownTick);
+        this._onPlaybackEvent(PlaybackEvent.SleepTimerChanged);
+    }
+
+    public getSleepTimerRemaining(): number {
+        return this._sleepTimerMillisecsLeft;
+    }
+
+    public cancelSleepTimer() {
+        this._log('cancelSleepTimer');
+        if (this._sleepTimer != undefined) {
+            clearInterval(this._sleepTimer);
+            this._sleepTimer = undefined;
+            this._sleepTimerMillisecsLeft = 0;
+            this._onPlaybackEvent(PlaybackEvent.SleepTimerChanged);
+        }
+    }
+
+    public pauseSleepTimer() {
+        if (this._sleepTimer != undefined) {
+            this._log('pauseSleepTimer');
+            this._sleepTimerPaused = true;
+        }
+    }
+
+    public resumeSleepTimer() {
+        if (this._sleepTimer != undefined) {
+            this._log('resumeSleepTimer');
+            this._sleepTimerPaused = false;
+        }
+    }
+
+    public setSeekIntervalSeconds(seconds: number) {
+        this._log('setSeekIntervalSeconds: '+ seconds);
+        this.seekIntervalSeconds = seconds;
+        // this.updateRemoteControlPreferredIntervals(seconds);
+    }
+
+    public destroy() {
+        this._log('destroy');
+        this.stop();
+        this.player = null;
+        this.delegate = null;
+        this.iosPlaylist = null;
+    }
+
+    //* PRIVATE HELPERS *//
 
     private _iosSeekTo(milisecs: number, completionHandler?: (boolean) => void) {
         const seekToSeconds = milisecs / 1000.0;
         this.player.seekToByAdaptingTimeToFitSeekableRangesToleranceBeforeToleranceAfterCompletionHandler(
                 seekToSeconds, false, kCMTimePositiveInfinity, kCMTimePositiveInfinity, (succeeded) => {
-            console.log(`Seek to ${milisecs} succeeded: ${succeeded}`);
+            this._log(`Seek to ${milisecs}ms succeeded: ${succeeded}`);
             if (completionHandler) {
                 completionHandler(succeeded);
             }
         });
     }
 
-    public _iosTimeUpdate(seconds: number) {
+    private _iosTimeUpdate(seconds: number) {
         this._onPlaybackEvent(PlaybackEvent.TimeChanged, Math.floor(seconds * 1000));
     }
 
-    public _iosFinishedPlaying(item: AudioItem) {
+    private _iosBufferingUpdate(item: AudioItem, earliestBufferSec: number, latestBufferSec: number) {
+        this._log(`bufferingUpdate  '${item.title}' now has buffered: ${earliestBufferSec}s - ${latestBufferSec}s`);
+    }
+
+    private _iosFinishedPlayingItem(item: AudioItem) {
         const finishedIndex = this.iosPlaylist.indexOfObject(item);
         this._onPlaybackEvent(PlaybackEvent.EndOfTrackReached, finishedIndex);
         if (finishedIndex >= this.iosPlaylist.count - 1) {
@@ -211,8 +308,8 @@ export class TNSAudioPlayer extends CommonAudioPlayer
         }
     }
 
-    public _iosPlayerChangedState(from: AudioPlayerState, to: AudioPlayerState) {
-        console.log(`_iosPlayerChangedState: ${from} -> ${to}`);
+    private _iosPlayerStateChanged(from: AudioPlayerState, to: AudioPlayerState) {
+        this._log(`stateChanged: ${from} -> ${to}`);
 
         switch(to) {
             case AudioPlayerState.Buffering: {
@@ -251,101 +348,8 @@ export class TNSAudioPlayer extends CommonAudioPlayer
                 break;
             }
             default: {
-                console.log(`Unknown state change ${from} -> ${to}`);
+                this._log(`unknown stateChange: ${from} -> ${to}`);
             }
         }
-    }
-    
-    public release() {
-        this.player.delegate = null;
-        this.player = null;
-        this.delegate = null;
-    }
-    
-    // public didChangeStateFromTo(fromState: LYTPlayerState, toState: LYTPlayerState) {
-    //     console.log("delegate.didChangeState: "+ fromState + " -> "+ toState);
-    // }
-    // public didFinishPlayingTrack(track: LYTAudioTrack) {
-    //     console.log("delegate.didFinishPlayingTrack", track.title);
-    //     this._onPlaybackEvent(PlaybackEvent.EndOfTrackReached);
-    // }
-    // public didFindDurationForTrack(duration: number, track: LYTAudioTrack) {
-    //     console.log("delegate.didFindDurationForTrack: "+ duration, track.title);
-    // }
-    // public didUpdateBufferedDurationForTrack(buffered: number, track: LYTAudioTrack) {
-    //     console.log("delegate.didUpdateBufferingForTrack: "+ buffered, track.title);
-    // }
-    // public didChangeToTrack(track: LYTAudioTrack) {
-    //     console.log("delegate.DidChangeToTrack", track.title);
-    // }
-    // public didEncounterError(error: NSError) {
-    //     console.log("delegate.didEncounterError: "+ error.localizedDescription);
-    // }
-    
-    private _sleepTimer: number;
-    private _sleepTimerPaused: boolean = false;
-    private _sleepTimerMillisecsLeft: number = 0;
-    
-    public setSleepTimer(millisecs: number) {
-        this.cancelSleepTimer();
-        
-        const countdownTick = 1000;
-        this._sleepTimerMillisecsLeft = millisecs;
-        this._sleepTimer = setInterval(() => {
-            if (!this._sleepTimerPaused && this.isPlaying()) {
-                this._sleepTimerMillisecsLeft = Math.max(this._sleepTimerMillisecsLeft - countdownTick, 0);
-                this._onPlaybackEvent(PlaybackEvent.SleepTimerChanged);
-            }
-            if (this._sleepTimerMillisecsLeft == 0) {
-                // Fade out volume and pause if not already paused.
-                if (this.isPlaying()) {
-                    // TODO: fade out volume
-                    //   this.fadeOutVolumeAndPause();
-                }
-                clearInterval(this._sleepTimer);
-                this._sleepTimer = undefined;
-            }
-        }, countdownTick);
-        this._onPlaybackEvent(PlaybackEvent.SleepTimerChanged);
-    }
-    
-    public getSleepTimerRemaining(): number {
-        return this._sleepTimerMillisecsLeft;
-    }
-    
-    public cancelSleepTimer() {
-        this._log('cancelSleepTimer');
-        if (this._sleepTimer != undefined) {
-            clearInterval(this._sleepTimer);
-            this._sleepTimer = undefined;
-            this._sleepTimerMillisecsLeft = 0;
-            this._onPlaybackEvent(PlaybackEvent.SleepTimerChanged);
-        }
-    }
-    
-    public pauseSleepTimer() {
-        if (this._sleepTimer != undefined) {
-            this._log('pauseSleepTimer');
-            this._sleepTimerPaused = true;
-        }
-    }
-    
-    public resumeSleepTimer() {
-        if (this._sleepTimer != undefined) {
-            this._log('resumeSleepTimer');
-            this._sleepTimerPaused = false;
-        }
-    }
-
-    public setSeekIntervalSeconds(seconds: number) {
-        this._log('setSeekIntervalSeconds: '+ seconds);
-        this.seekIntervalSeconds = seconds;
-        // this.updateRemoteControlPreferredIntervals(seconds);
-    }
-    
-    public destroy() {
-        this._log('destroy');
-        this.stop();
-        this.release()
     }
 }
